dfb3582093b86996d7b67217a365837201853776
Cesar Herrera <cesarherr321@gmail.com>
2023-11-13 14:58:37 -0300

    Add computer component

---
+++ src/components/Computer.jsx
@@ -0,0 +1,14 @@
+function Computer() {
+  return(
+    <section className="flex justify-center items-center h-screen
+    bg-blue-100bg-blue-100 leftSide" 
+    id="computer">
+        <img />
+      <div className="flex justify-center items-center w-96 bg-pink-300">
+        <iframe src="https://giphy.com/embed/3oEdvbRHem1psqd7a0" className="giphy-embed w-96"></iframe>
+      </div>
+    </section>
+  );
+}
+
+export default Computer;
