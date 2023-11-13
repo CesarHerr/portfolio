bf9fb43238ff6094f75846834f1e5d36cc9aa60f
Cesar Herrera <cesarherr321@gmail.com>
2023-11-13 14:59:05 -0300

    Add projects Component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,33 @@
+import carpet from '../assets/carpet.png';
+
+function Projects() {
+  return(
+    <section className="flex flex-col justify-center items-center h-screen
+     gap-4 leftSide relative font-bold" 
+    id="computer">
+      <h2 className="text-6xl absolute top-2" id="projects">Projects</h2>
+        <div className="flex justify-center items-center w-full gap-10">
+          <img src={carpet} className="w-3/12" />
+          <p className="w-5/12 text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className="flex justify-center items-center w-full gap-10">
+        <p className="w-5/12 text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <img src={carpet} className="w-3/12" />
+        </div>
+        <div className="flex justify-center items-center w-full gap-10">
+          <img src={carpet} className="w-3/12" />
+          <p className="w-5/12 text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className="flex justify-center items-center w-full gap-10">
+        <p className="w-5/12 text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <img src={carpet} className="w-3/12" />
+        </div>
+    </section>
+  );
+}
+
+export default Projects;
