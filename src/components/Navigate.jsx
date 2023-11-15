1cba935b1f3cc364dc0e387634a9cc9dfea4c75e
Cesar Herrera <cesarherr321@gmail.com>
2023-11-14 21:18:04 -0300

    Refactor navigate component

--- src/components/Navigate.jsx
+++ src/components/Navigate.jsx
@@ -1,15 +1,16 @@
 function Navigate() {
   return (
     <section className="flex flex-col items-end fixed mt-32 right-10 rightSide">
-      <h2 className="w-full text-left text-4xl">Hey There.</h2>
-      <h2 className="w-full text-left text-4xl">I&apos;m a Software Developer</h2>
+      <h2 className="w-full text-left text-4xl font-dancing">Hey There.</h2>
+      <h2 className="w-full text-left text-4xl font-dancing">I'm César.</h2>
+      <h3 className="w-full text-left text-3xl mt-4">I&apos;m a Software Developer</h3>
       <p className="pt-10 w-full text-left"> I can help you build a product , feature or website Look through some of my work and 
         experience! If you like what you see and have a project you need coded, 
         don&apos;t hesitate to contact me.</p>
-      <nav className="flex flex-col items-start pt-32 leading-8 w-2/4">
+      <nav className="flex flex-col items-start pt-32 leading-8 w-4/5">
         <a href="#computer"> ◀️ About</a>
-        <a href="#projects"> ◀️ Projects</a>
-        <a href="#skills"> ◀️ Skills</a>
+        <a href="#projects" className="my-6"> ◀️ Projects</a>
+        <a href="#skills" className="mb-6"> ◀️ Skills</a>
         <a href="#contactForm"> ◀️ Contact</a>
       </nav>
     </section>
