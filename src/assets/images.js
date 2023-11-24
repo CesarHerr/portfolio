a776aa54706358bb0fc15b551ce39fe3e818426b
Cesar Herrera <cesarherr321@gmail.com>
2023-11-23 23:53:07 -0300

    Update icons and images.js

---
+++ src/assets/images.js
@@ -0,0 +1,38 @@
+import carpet from './carpet.png';
+import desktop7 from './desktop7.png';
+import github from './github.png';
+import hiking from './hiking.png';
+import lamp from './lamp.png';
+import lamp2 from './lamp2.png';
+import lamp3 from './lamp3.png';
+import linkedin from './linkedin.png';
+import myAvatar from './myAvatar2.png';
+import paids from './paids.png';
+import recipe from './recipe.png';
+import rfad2 from './rfad2.png';
+import space from './space.png';
+import x from './x.png';
+import zelda from './zelda.png';
+import rspec from './rspec.svg';
+
+const images = {
+  carpet,
+  desktop7,
+  github,
+  hiking,
+  lamp,
+  lamp2,
+  lamp3,
+  linkedin,
+  myAvatar,
+  paids,
+  recipe,
+  rfad2,
+  space,
+  x,
+  zelda,
+  rspec
+};
+
+
+export  default images;
