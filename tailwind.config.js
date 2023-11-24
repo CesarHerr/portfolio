14d0159bf125f63713cb6462f965dacbe5c7bb87
Cesar Herrera <cesarherr321@gmail.com>
2023-11-23 23:51:01 -0300

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ export default {
   ],
   theme: {
     extend: {
+      animation: {
+        'pulse-slow': 'pulse 1s linear infinite',
+      },
+      boxShadow: {
+        'myShadow': '8px 8px 0px 0px rgb(64, 64, 64)',
+      },
       fontFamily: {
         'dancing': ['Dancing Script', 'cursive'],
       },
+      colors: {
+        myPurple: '#AE8FDB',
+        myBlue: '#4C3A69',
+        myYellow: '#FEF8B4',
+        myGray: '#404040',
+        myGreen: '#6FDCBF',
+        myBrown: '#29272A',
+      }
     },
   },
   plugins: [],
