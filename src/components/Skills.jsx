2aa24da101188ec04f88ee3a38d68de277b8c7a4
Cesar Herrera <cesarherr321@gmail.com>
2023-11-25 16:32:24 -0300

    Add skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,38 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useInView } from 'react-intersection-observer';
+import Language from './Language';
+import Frameworks from './Frameworks';
+import { setOnSkills } from '../redux/projectSlice';
+
 function Skills() {
-  return(
-    <section className="flex flex-col justify-center items-center h-screen gap-4 leftSide relative font-bold" id="skills">
-      <h2 className="s text-6xl absolute top-4">Skills</h2>
-      <ul className="flex flex-col w-10/12" id="knowledge">
-        <li className="flex">
-            <ul className="leading-10 flex-col">
-                <li className="list-icon"><h3 className="text-3xl font-bold">Language</h3></li>
-                <li><p>JavaScript</p></li>
-                <li><p>Ruby</p></li>
-                <li><p>HTML</p></li>
-                <li><p>CSS</p></li>
-            </ul>
-        </li>
-        <li className="flex flex-row-reverse">
-            <ul className="leading-10">  
-                <li className="list-icon"><h3 className="text-3xl font-bold">Frameworks</h3></li>                            
-                <li><p>React.js</p></li>
-                <li><p>Ruby on Rails</p></li>
-                <li><p>RSpec</p></li>
-                <li><p>Capybara</p></li>  
-                <li><p>Selenium</p></li>                       
-            </ul>
-        </li>
-        <li className="flex">
-            <ul className="leading-10">    
-                <li className="list-icon"><h3 className="text-3xl font-bold">Skills</h3></li>
-                <li><p>Database Management</p></li>
-                <li><p>Version Control</p></li>
-                <li><p>CLI</p></li>
-                <li><p>API Design</p></li>
-                <li><p>Web Development</p></li>                                 
-            </ul>
-        </li>
-      </ul>
+  const dispatch = useDispatch();
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    dispatch(setOnSkills(inView));
+  }, [inView, dispatch]);
+
+  return (
+    <section className="h-screen w-full md:ml-[22%] xl:ml-[23%] bg-myYellow md:bg-transparent" id="skills">
+      <h2 ref={ref} className={`text-4xl md:text-6xl font-bold pb-28 mt-14 ${inView ? 'titleAnimation' : 'opacity-0'}`}>Tech Skills</h2>
+      <div className="grid place-content-center h-3/5">
+        <div className="grid h-96">
+          <Language  />
+          <Frameworks />
+        </div>
+      </div>
     </section>
   );
 }
