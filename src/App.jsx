f1d704fea45e7838ec929762c6a7d023f1f45b1c
Cesar Herrera <cesarherr321@gmail.com>
2023-11-25 16:36:18 -0300

    Update app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import { useSelector } from 'react-redux';
 import './App.css'
 import Navigate from './components/Navigate';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import ContactForm from './components/ContactForm';
+import Popup from './components/Popup';
+import TicTacToe from './components/TicTacToe';
 
 function App() {
+  const { project } = useSelector((state) => state.project);
 
   return (
-    <section className="flex flex-col max-w-screen-xl mx-auto relative">
+    <section className="flex flex-col max-w-screen-xl mx-auto">
       <Navigate />
+      <TicTacToe />      
       <Projects />
       <Skills />
       <ContactForm />
+      {project !== null && <Popup />}
     </section>
   )
 }
