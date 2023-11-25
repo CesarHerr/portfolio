1823b9543d3328c5edd25ff459f64f543c9655aa
Cesar Herrera <cesarherr321@gmail.com>
2023-11-25 15:42:44 -0300

    Change extensions for images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,19 +1,18 @@
 import carpet from './carpet.png';
 import desktop7 from './desktop7.png';
 import github from './github.png';
-import hiking from './hiking.png';
+import hiking from './hiking.webp';
 import lamp from './lamp.png';
 import lamp2 from './lamp2.png';
 import lamp3 from './lamp3.png';
 import linkedin from './linkedin.png';
-import myAvatar from './myAvatar2.png';
-import paids from './paids.png';
-import recipe from './recipe.png';
-import rfad2 from './rfad2.png';
-import space from './space.png';
+import myAvatar from './myAvatar2.webp';
+import paids from './paids.webp';
+import recipe from './recipe.webp';
+import rfad2 from './rfad2.webp';
+import space from './space.webp';
 import x from './x.png';
-import zelda from './zelda.png';
-import rspec from './rspec.svg';
+import zelda from './zelda.webp';
 
 const images = {
   carpet,
@@ -31,7 +30,6 @@ const images = {
   space,
   x,
   zelda,
-  rspec
 };
 
 
