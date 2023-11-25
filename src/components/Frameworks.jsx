80b4b3c8aaf4be6299dbffbd0ddf68ad2e53e2f9
Cesar Herrera <cesarherr321@gmail.com>
2023-11-25 16:27:42 -0300

    Add animation for framework icons

---
+++ src/components/Frameworks.jsx
@@ -0,0 +1,96 @@
+import '../styles/frameworks.css';
+import { DiReact } from 'react-icons/di';
+import { SiRedux } from "react-icons/si";
+import { SiBootstrap } from "react-icons/si";
+import { DiSass } from "react-icons/di";
+import { SiTailwindcss } from "react-icons/si";
+import { SiRubyonrails } from "react-icons/si";
+import { SiRubygems } from "react-icons/si";
+import { SiJest } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+
+function Frameworks() {
+
+  return (
+    <>
+      <div className="sliderB">
+        <div className="slide-trackB">  
+          <div className="slideB">
+            <DiReact />
+            <p>React.js</p>
+          </div>
+          <div className="slideB">
+            <SiRedux />
+            <p>Redux</p>
+          </div>
+          <div className="slideB">
+            <SiBootstrap />
+            <p>Bootstrap</p>
+          </div>
+          <div className="slideB">
+            <DiSass />
+            <p>SASS</p>
+          </div>
+          <div className="slideB">
+            <SiTailwindcss />
+            <p>Tailwind</p>
+          </div>
+          <div className="slideB">
+            <SiRubyonrails />
+            <p>Rails</p>
+          </div>
+          <div className="slideB">
+            <SiRubygems />
+            <p>RSpec</p>
+          </div>
+          <div className="slideB">
+            <SiJest />
+            <p>Jest</p>
+          </div>
+          <div className="slideB">
+            <SiPostgresql />
+            <p>PostgreSQL</p>
+          </div>
+          <div className="slideB">
+            <DiReact />
+            <p>React.js</p>
+          </div>
+          <div className="slideB">
+            <SiRedux />
+            <p>Redux</p>
+          </div>
+          <div className="slideB">
+            <SiBootstrap />
+            <p>Bootstrap</p>
+          </div>
+          <div className="slideB">
+            <DiSass />
+            <p>SASS</p>
+          </div>
+          <div className="slideB">
+            <SiTailwindcss />
+            <p>Tailwind</p>
+          </div>
+          <div className="slideB">
+            <SiRubyonrails />
+            <p>Rails</p>
+          </div>
+          <div className="slideB">
+            <SiRubygems />
+            <p>RSpec</p>
+          </div>
+          <div className="slideB">
+            <SiJest />
+            <p>Jest</p>
+          </div>
+          <div className="slideB">
+            <SiPostgresql />
+            <p>PostgreSQL</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Frameworks;
