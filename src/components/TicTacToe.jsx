fab6726a5d19f723d0a73417e5193bb0ab854ded
Cesar Herrera <cesarherr321@gmail.com>
2023-11-26 16:27:50 -0300

    Add ticTacToe to about

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -7,8 +7,8 @@ import PropTypes from "prop-types";
 import { setOnAbout } from "../redux/projectSlice";
 
 const Square = ({ children, updateBoard, isSelected, index }) => {
-  const className = `square inline text-3xl w-16 h-16 border-4 border-myGray rounded-md cursor-pointer
-                    grid place-items-center ${isSelected ? `bg-myGreen opacity-80` : ""}`;
+  const className = `square inline text-sm w-8 h-8 border border-myGray rounded-sm cursor-pointer
+                    grid place-items-center ${isSelected ? `bg-myYellow` : ""}`;
 
   const handleClick = () => {
     updateBoard(index);
@@ -123,17 +123,15 @@ function TicTacToe() {
   }, [inViewAbout, dispatch]);
 
   return (
-    <section className="absolute md:static bottom-[-800px] bg-myYellow md:bg-transparent w-full md: h-screen mb-14 md:ml-[23%] lg:ml-[17%] 2xl:ml-[23%]" id="playGround">
-      <h2 className="text-4xl md:text-5xl font-extrabold mt-14">PlayGround</h2>
+    <section className="w-fit mx-auto" id="playGround">
       <div ref={refAbout} className="flex mt-10 justify-center">
-        <h3 className="text-3xl font-bold text-myBlue -rotate-12">It&apos;s your turn !</h3>
-        <div>
-          <p className="text-xl md:text-2xl font-bold text-myBlue rotate-12 a&apos;nimate-pulse curs">Come on is easy !</p>
-          <p className="text-xl md:text-2xl font-bold text-myBlue rotate-12 animate-pulse-slow">Good Luck !</p>
+        <div className="flex justify-center gap-2 w-full">
+          <p className="text-xs font-bold text-myBlue -rotate-12 animate-pulse curs">It&apos;s your turn !</p>
+          <p className="text-xs font-bold text-myBlue rotate-12 animate-pulse-slow">Come on is easy !</p>
         </div>
       </div>
       <div className="grid place-content-center">
-        <div className="grid grid-cols-3 gap-3 my-10 place-items-center ">
+        <div className="rotate-12 grid grid-cols-3 gap-1 mt-3 mb-7 place-items-center ">
           {board.map((square, index) => {
             return (
               <Square key={index} index={index} updateBoard={updateBoard}>
@@ -143,13 +141,10 @@ function TicTacToe() {
           })}
         </div>
       </div>
-      <div className="flex p-6 text-2xl justify-center">
+      <div className="flex p-1 justify-center gap-2">
         <Square isSelected={turn === turns.X}>{turns.X}</Square>
         <Square isSelected={turn === turns.O}>{turns.O}</Square>
       </div>
-      <button type="button" onClick={resetGame} className="bg-myYellow p-4 w-2/4 md:w-1/4 font-bold active:bg-myGray active:text-white
-      shadow-myShadow border border-myGray hover:translate-x-2 hover:translate-y-2 hover:shadow-none transition-all">Reset Game</button>
-
       {winner !== null && (
         <div className="fixed w-full h-screen top-0 left-0 grid place-items-center bg-myBlue z-50 opacity-90">
           <div className="text-4xl text-myYellow flex flex-col justify-around h-3/6">
