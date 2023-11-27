67b710db91fa6634888ac19acca6ce0ed6a1b230
Cesar Herrera <cesarherr321@gmail.com>
2023-11-27 12:39:35 -0300

    Change width about and use prettier

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,33 @@
-import images from "../assets/images";
-import TicTacToe from "./TicTacToe";
+import images from '../assets/images';
+import TicTacToe from './TicTacToe';
 
-function About () {
+function About() {
   return (
-    <div className="h-screen relative hidden md:inline items-end gap-4 pr-4 xl:pr-10 2xl:pr-0 ml-[30%]
-    lg:ml-[20%] xl:ml-[20%] 2xl:ml-[35%] mb-24 overflow-hidden" id="about">
-      <h3 className="w-9/12 text-left ml-[25%] text-3xl lg:text-4xl mt-64 text-myBlue">I&apos;m a Full Stack Developer</h3>
-      <p className="w-9/12 text-left  ml-[25%] text-xl "> With a background in database administration, and technical
-        support. In the last year I spent 1300+ hours studying and working in a remote environment, in pair
-        programming, using GitHub, industry-standard git-flow, and daily stand ups to communicate and collaborate
-        with international remote developers.</p>
+    <section
+      className="h-screen relative hidden items-end gap-2 pr-4 md:inline xl:pr-20
+      md:ml-[20%] xl:ml-[30%] mb-24 overflow-hidden bg-green-300"
+      id="about"
+    >
+      <h3 className="w-9/12 text-left ml-[25%] text-3xl lg:text-4xl mt-64 text-myBlue">
+        I&apos;m a Full Stack Developer
+      </h3>
+      <p className="w-9/12 text-left  ml-[25%] text-xl ">
+        {' '}
+        With a background in database administration, and technical support. In
+        the last year I spent 1300+ hours studying and working in a remote
+        environment, in pair programming, using GitHub, industry-standard
+        git-flow, and daily stand ups to communicate and collaborate with
+        international remote developers.
+      </p>
       <div className="w-[210px] h-[260px] opacity-80 2xl:pt-16 absolute right-4">
-        <img src={images.post} className="w-[210px] h-[220px] rotate-6 absolute -z-10" />
-        <TicTacToe /> 
+        <img
+          src={images.post}
+          className="w-[210px] h-[220px] rotate-6 absolute -z-10"
+        />
+        <TicTacToe />
       </div>
-    </div>
-  )
+    </section>
+  );
 }
 
 export default About;
