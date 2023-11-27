a7401e769e0dc52820a7485175cdaaad8052d8af
Cesar Herrera <cesarherr321@gmail.com>
2023-11-27 12:57:56 -0300

    Use prettier on Navigate component

--- src/components/Navigate.jsx
+++ src/components/Navigate.jsx
@@ -1,52 +1,127 @@
-import { useSelector } from "react-redux";
-import { RiTwitterXLine } from "react-icons/ri";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FiGithub } from "react-icons/fi";
+import { useSelector } from 'react-redux';
+import { RiTwitterXLine } from 'react-icons/ri';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FiGithub } from 'react-icons/fi';
 import images from '../assets/images';
 
 function Navigate() {
-  const { onProject, onAbout, onSkills, onContact } = useSelector((state) => state.project);
+  const { onProject, onAbout, onSkills, onContact } = useSelector(
+    (state) => state.project
+  );
 
   return (
-    <aside className="flex flex-col w-full md:w-[20%] xl:w-[30%] items-start justify-center gap-5 
-      md:fixed h-screen bg-myYellow min-w-[330px] z-10 relative">
-      <header className="flex justify-start items-end ml-10 w-full max-w-[400px] relative top-2 md:flex-col md:justify-center 
-      md:gap-0 md:ml-10 md:items-center">
+    <aside
+      className="flex flex-col w-full md:w-[20%] xl:w-[30%] items-start justify-center gap-5 
+      md:fixed h-screen bg-myYellow min-w-[330px] z-10 relative"
+    >
+      <header
+        className="flex justify-start items-end ml-10 w-full max-w-[400px] relative top-2 md:flex-col md:justify-center 
+      md:gap-0 md:ml-10 md:items-center"
+      >
         <div className="min-w-[250px] relative z-20">
-          <h1 className="text-left text-5xl xl:text-6xl 2xl:text-7xl font-dancing font-bold -rotate-12 w-full animation">Hey There.</h1>
-          <h2 className="text-left text-5xl xl:text-6xl 2xl:text-7xl font-dancing font-bold -rotate-12 mb-7 w-full">I&apos;m César.</h2>
+          <h1 className="text-left text-5xl xl:text-6xl 2xl:text-7xl font-dancing font-bold -rotate-12 w-full animation">
+            Hey There.
+          </h1>
+          <h2 className="text-left text-5xl xl:text-6xl 2xl:text-7xl font-dancing font-bold -rotate-12 mb-7 w-full">
+            I&apos;m César.
+          </h2>
         </div>
-        <img src={images.myAvatar} alt="avatar" className="w-36 h-36 rounded-full border-4 border-myGray
-         bg-myPurple z-0"/>
+        <img
+          src={images.myAvatar}
+          alt="avatar"
+          className="w-36 h-36 rounded-full border-4 border-myGray
+         bg-myPurple z-0"
+        />
       </header>
       <section>
-      <h3 className="text-left text-2xl pt-5 w-10/12 ml-10 text-myBlue md:hidden">I&apos;m a Full Stack Developer</h3>
-      <p className="w-10/12 text-left text-md ml-10 md:hidden"> With a background in database administration, and technical
-        support. In the last year I spent 1300+ hours studying and working in a remote environment, in pair
-        programming, using GitHub, industry-standard git-flow, and daily stand ups to communicate and collaborate
-        with international remote developers.</p>
-        <p className="hidden md:block pt-5 w-10/12 md:w-9/12 text-left ml-10 md:ml-14 text-lg"> I can help you build a product , feature or website. Look through some of my work and 
-        experience! </p>
-        <nav className="flex items-center w-full bg-myPurple md:bg-transparent md:bg-none justify-around md:flex-col fixed md:relative md:items-start 
-          top-0 md:pt-5 leading-8 md:w-4/5 md:ml-14 text-xl z-40">
-          <a href="/" className={`font-bold ${onAbout ? 'animatedBack active' : 'animatedOut reverse'}`}>
-            <hr className={`hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3 ${onAbout ? '' : ''}`}/> Me </a>
-          <a href="#projects" className={`font-bold md:my-2 ${onProject ? 'animatedBack active' : 'animatedOut reverse'}`}>
-            <hr className="hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3"/> Projects</a>
-          <a href="#skills" className={`font-bold md:my-2 ${onSkills ? 'animatedBack active' : 'animatedOut reverse'}`}>
-            <hr className="hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3"/> Skills</a>
-          <a href="#contactForm" className={`font-bold ${onContact ? 'animatedBack active' : 'animatedOut reverse'}`}>
-            <hr className="hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3"/> Contact</a>
+        <h3 className="text-left text-2xl pt-5 w-10/12 ml-10 text-myBlue md:hidden">
+          I&apos;m a Full Stack Developer
+        </h3>
+        <p className="w-10/12 text-left text-md ml-10 md:hidden">
+          {' '}
+          With a background in database administration, and technical support.
+          In the last year I spent 1300+ hours studying and working in a remote
+          environment, in pair programming, using GitHub, industry-standard
+          git-flow, and daily stand ups to communicate and collaborate with
+          international remote developers.
+        </p>
+        <p className="hidden md:block pt-5 w-10/12 md:w-9/12 text-left ml-10 md:ml-14 text-lg">
+          {' '}
+          I can help you build a product , feature or website. Look through some
+          of my work and experience!{' '}
+        </p>
+        <nav
+          className="flex items-center w-full bg-myPurple md:bg-transparent md:bg-none justify-around md:flex-col fixed md:relative md:items-start 
+          top-0 md:pt-5 leading-8 md:w-4/5 md:ml-14 text-xl z-40"
+        >
+          <a
+            href="/"
+            className={`font-bold ${
+              onAbout ? 'animatedBack active' : 'animatedOut reverse'
+            }`}
+          >
+            <hr
+              className={`hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3 ${
+                onAbout ? '' : ''
+              }`}
+            />{' '}
+            Me{' '}
+          </a>
+          <a
+            href="#projects"
+            className={`font-bold md:my-2 ${
+              onProject ? 'animatedBack active' : 'animatedOut reverse'
+            }`}
+          >
+            <hr className="hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3" />{' '}
+            Projects
+          </a>
+          <a
+            href="#skills"
+            className={`font-bold md:my-2 ${
+              onSkills ? 'animatedBack active' : 'animatedOut reverse'
+            }`}
+          >
+            <hr className="hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3" />{' '}
+            Skills
+          </a>
+          <a
+            href="#contactForm"
+            className={`font-bold ${
+              onContact ? 'animatedBack active' : 'animatedOut reverse'
+            }`}
+          >
+            <hr className="hidden md:inline border-1 border-myGray px-2  mx-2 relative top-3" />{' '}
+            Contact
+          </a>
         </nav>
       </section>
       <footer className="flex md:w-full md:justify-center gap-4 p-1 md:py-2 fixed md:absolute bottom-2 right-2 md:right-auto bg-myGray rounded">
-        <a className="cursor-pointer hover:scale-110" href="https://twitter.com/Cesarherr2" target="_blank" rel="noreferrer" aria-label="go to my twitter account">
-          <RiTwitterXLine className=" hover:text-purple-800 w-5 md:w-10 text-white"/>
+        <a
+          className="cursor-pointer hover:scale-110"
+          href="https://twitter.com/Cesarherr2"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="go to my twitter account"
+        >
+          <RiTwitterXLine className=" hover:text-purple-800 w-5 md:w-10 text-white" />
         </a>
-        <a className="cursor-pointer hover:scale-110" href="https://www.linkedin.com/in/cesarherr/" target="_blank" rel="noreferrer" aria-label="go to my linkedin account">
+        <a
+          className="cursor-pointer hover:scale-110"
+          href="https://www.linkedin.com/in/cesarherr/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="go to my linkedin account"
+        >
           <FaLinkedinIn className=" hover:text-purple-800 w-5 md:w-10 text-white" />
         </a>
-        <a className="cursor-pointer hover:scale-110" href="https://cesarherr.github.io/portfolio_1.2/" target="_blank" rel="noreferrer" aria-label="go to my github account">
+        <a
+          className="cursor-pointer hover:scale-110"
+          href="https://cesarherr.github.io/portfolio_1.2/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="go to my github account"
+        >
           <FiGithub className=" hover:text-purple-800 w-5 md:w-10 text-white" />
         </a>
       </footer>
