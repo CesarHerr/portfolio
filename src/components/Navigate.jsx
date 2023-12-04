60b4df67347b93768b97b0dde8439385af1086f6
Cesar Herrera <cesarherr321@gmail.com>
2023-12-03 23:39:13 -0300

    Update localStorage and domain

--- src/components/Navigate.jsx
+++ src/components/Navigate.jsx
@@ -30,14 +30,8 @@ function Navigate() {
     }
   }, [theme]);
 
-  if (
-    localStorage.theme === 'dark' ||
-    (!('theme' in localStorage) &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches)
-  ) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
+  if ('theme' in localStorage) {
+    localStorage.theme = theme;
   }
 
   return (
